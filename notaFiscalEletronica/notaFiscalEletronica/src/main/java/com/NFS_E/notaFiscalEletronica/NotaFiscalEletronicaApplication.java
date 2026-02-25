fe2d3992e934dccd38862b8d846b89767a8e4d90
joao <jovmamikl@gmail.com>
2026-02-25 08:52:50 -0300

    Inicital commit for NFs-E project

---
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/NotaFiscalEletronicaApplication.java
@@ -0,0 +1,13 @@
+package com.NFS_E.notaFiscalEletronica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotaFiscalEletronicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotaFiscalEletronicaApplication.class, args);
+	}
+
+}
---
+++ notaFiscalEletronica/notaFiscalEletronica/src/test/java/com/NFS_E/notaFiscalEletronica/NotaFiscalEletronicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NFS_E.notaFiscalEletronica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotaFiscalEletronicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
