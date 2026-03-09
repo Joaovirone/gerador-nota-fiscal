2fae470abc1c2d19ca0240cf79435b2476de1e52
joao <jovmamikl@gmail.com>
2026-03-09 15:23:23 -0300

    feat: implementada anotações para validações do DTO para tipo Email e tamanho da senha

--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/controller/dto/AuthDTO.java
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/controller/dto/AuthDTO.java
@@ -1,15 +1,19 @@
 package com.NFS_E.notaFiscalEletronica.controller.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AuthDTO {
     
     
-    @NotBlank(message="Login não pode ser nulo")
+    @NotBlank(message="Login é obrigatório")
+    @Email(message= "O login deve ser um email válido")
     private String login;
     
-    @NotBlank(message="Senha não pode ser nula")
+    @NotBlank(message="Senha é obrigatória")
+    @Size(min=8, message= "A senha deve conter no mínimo 8 caracteres")
     private String senha;
 }
