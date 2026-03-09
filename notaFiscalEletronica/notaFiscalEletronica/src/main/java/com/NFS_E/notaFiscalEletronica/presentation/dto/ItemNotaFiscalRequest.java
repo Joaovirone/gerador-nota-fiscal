d9fede165559bf0ac3f1238995e22961de413d66
joao <jovmamikl@gmail.com>
2026-03-09 10:33:34 -0300

    clean: arquivos que estavam conflitando foram apagados

--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/presentation/dto/ItemNotaFiscalRequest.java
+++
@@ -1,9 +0,0 @@
-package com.NFS_E.notaFiscalEletronica.presentation.dto;
-
-import java.math.BigDecimal;
-
-public record ItemNotaFiscalRequest(
-    String descricao,
-    BigDecimal quantidade,
-    BigDecimal valorUnitario
-) {}
--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/presentation/dto/NotaFiscalRequest.java
+++
@@ -1,8 +0,0 @@
-package com.NFS_E.notaFiscalEletronica.presentation.dto;
-
-import java.util.List;
-
-public record NotaFiscalRequest(
-    String notaFiscalDestino,
-    List<ItemNotaFiscalRequest> itens
-) {}
