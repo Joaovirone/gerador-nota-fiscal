c09a066af519f9aa3dc3e78741c8f2a7c49a29c5
joao <jovmamikl@gmail.com>
2026-03-09 14:44:10 -0300

    feat: criado entidade usuário para existir login

---
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/entity/Usuario.java
@@ -0,0 +1,75 @@
+package com.NFS_E.notaFiscalEletronica.entity;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name="login",unique = true, nullable = false)
+    private String login;
+
+
+    @Column(name="senha",nullable = false)
+    private String senha;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() { 
+        return senha; 
+    }
+
+    @Override
+    public String getUsername() { 
+        return login; 
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { 
+        return true; 
+    }
+
+    @Override
+    public boolean isAccountNonLocked() { 
+        return true; 
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() { 
+        return true; 
+    }
+
+    @Override
+    public boolean isEnabled() { 
+        return true; 
+    } 
+}
