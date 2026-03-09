e8756e37ec9eeaf6bc2ef2ce74f34fdab27c3e10
joao <jovmamikl@gmail.com>
2026-03-09 15:22:54 -0300

    feat: implementado endpoint para cadastratr usuario

--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/controller/AutenticacaoController.java
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/controller/AutenticacaoController.java
@@ -1,6 +1,7 @@
 package com.NFS_E.notaFiscalEletronica.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.NFS_E.notaFiscalEletronica.controller.dto.AuthDTO;
 import com.NFS_E.notaFiscalEletronica.entity.Usuario;
 import com.NFS_E.notaFiscalEletronica.infra.security.TokenService;
+import com.NFS_E.notaFiscalEletronica.service.UsuarioService;
 
 import jakarta.validation.Valid;
 
@@ -23,9 +25,12 @@ public class AutenticacaoController {
     private AuthenticationManager manager;
 
     @Autowired
+    private UsuarioService usuarioService;
+
+    @Autowired
     private TokenService tokenService;
 
-    @PostMapping("/login")
+    @PostMapping("/login-usuario")
     public ResponseEntity efetuarLogin(@RequestBody @Valid AuthDTO dados) {
         var authenticationToken = new UsernamePasswordAuthenticationToken(dados.getLogin(), dados.getSenha());
         var authentication = manager.authenticate(authenticationToken);
@@ -33,6 +38,15 @@ public class AutenticacaoController {
         var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
 
         return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+    }
+    
+    @PostMapping("/cadastrar-usuario")
+    public ResponseEntity cadastrar(@RequestBody @Valid AuthDTO dto) {
+        
+        usuarioService.cadastrarUsuario(dto);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+        
     }
     
    
