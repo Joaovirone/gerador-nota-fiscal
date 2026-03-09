43596faf9a02d9b93071c4c101f7d1f20afa3557
joao <jovmamikl@gmail.com>
2026-03-09 12:06:23 -0300

    feat: implementada função para procurar o número maximo de notas fiscais utilzando Query direto do JPA

--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/repository/NotaFiscalRepository.java
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/repository/NotaFiscalRepository.java
@@ -2,10 +2,13 @@ package com.NFS_E.notaFiscalEletronica.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.NFS_E.notaFiscalEletronica.entity.NotaFiscal;
 
 public interface NotaFiscalRepository extends JpaRepository <NotaFiscal, UUID>{
     
+    @Query("SELECT max(n.numero) FROM NotaFiscal n" )
+    Long findMaxNumero();
 }
