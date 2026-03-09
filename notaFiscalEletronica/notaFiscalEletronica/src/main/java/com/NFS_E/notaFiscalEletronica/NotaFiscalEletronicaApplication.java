52860f45fbc3fade269a58c0c1e586ded10c4660
joao <jovmamikl@gmail.com>
2026-03-09 12:00:45 -0300

    feat: implementada auditoria no projeto

--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/NotaFiscalEletronicaApplication.java
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/NotaFiscalEletronicaApplication.java
@@ -2,8 +2,10 @@ package com.NFS_E.notaFiscalEletronica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NotaFiscalEletronicaApplication {
 
 	public static void main(String[] args) {
