135be6c4d5383b88619fc4a71153746163b5a119
joao <jovmamikl@gmail.com>
2026-03-09 10:31:07 -0300

    feat: criado modelmapper da ntoa fiscal para diminuir sujeira no código

--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/controller/dto/mapper/NotaFiscalMapper.java
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/controller/dto/mapper/NotaFiscalMapper.java
@@ -3,6 +3,7 @@ package com.NFS_E.notaFiscalEletronica.controller.dto.mapper;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import com.NFS_E.notaFiscalEletronica.controller.dto.ItemNotaFiscalResponseDTO;
@@ -11,47 +12,24 @@ import com.NFS_E.notaFiscalEletronica.controller.dto.NotaFiscalResponseDTO;
 import com.NFS_E.notaFiscalEletronica.entity.ItemNotaFiscal;
 import com.NFS_E.notaFiscalEletronica.entity.NotaFiscal;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class NotaFiscalMapper {
+
+    private final ModelMapper modelMapper;
     
     public NotaFiscal toEntity(NotaFiscalRequestDTO dto){
 
-        NotaFiscal nota =new NotaFiscal();
-
-        nota.setNotaFiscalDestino(dto.notaFsicalDestino());
-
-        List<ItemNotaFiscal> itens = dto.itens().stream()
-        .map(itemDto -> {
-            ItemNotaFiscal item = new ItemNotaFiscal();
-            item.setDescricao(itemDto.descricao());
-            item.setQuantidade(itemDto.quantidade());
-            item.setValorUnitario(itemDto.valorUnitario());
-
-            return item;
-        }).collect(Collectors.toList());
-
-        nota.setItens(itens);
-        return nota;
+        return modelMapper.map(dto, NotaFiscal.class);
     }
 
     public NotaFiscalResponseDTO toResponse(NotaFiscal entity){
 
-        List<ItemNotaFiscalResponseDTO> itensDto = entity.getItens().stream()
-        .map(item -> new ItemNotaFiscalResponseDTO(
-            item.getId(), 
-            item.getDescricao(),
-            item.getQuantidade(), 
-            item.getValorUnitario(), 
-            item.getValorTotal()
-        )).toList();
-
-        return new NotaFiscalResponseDTO(
-            entity.getId(),
-            entity.getNotaFiscalDestino(),
-            entity.getValorTotal(),
-            entity.getStatus().name(),
-            itensDto
-        );
+        return modelMapper.map(entity, NotaFiscalResponseDTO.class);
 
     }
 }   
