f74347b2c5429a4c6c7c42b56118797f6882f386
joao <jovmamikl@gmail.com>
2026-04-27 10:27:00 -0300

    fix: correct import statement for Query annotation in NotaFiscalRepository

--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/repository/NotaFiscalRepository.java
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/repository/NotaFiscalRepository.java
@@ -2,9 +2,9 @@ package com.NFS_E.notaFiscalEletronica.repository;
 
 import java.util.UUID;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import com.NFS_E.notaFiscalEletronica.entity.NotaFiscal;
 
